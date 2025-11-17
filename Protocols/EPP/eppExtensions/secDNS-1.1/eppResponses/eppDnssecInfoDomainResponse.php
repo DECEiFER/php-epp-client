0fe7b6cea100fe8a2e37a17179089b78b0973538
DECEiFER <oneill.dav@gmail.com>
2025-11-17 17:16:55 +0000

    Add getSecdns method to retrieve keyData elements within the same object

--- Protocols/EPP/eppExtensions/secDNS-1.1/eppResponses/eppDnssecInfoDomainResponse.php
+++ Protocols/EPP/eppExtensions/secDNS-1.1/eppResponses/eppDnssecInfoDomainResponse.php
@@ -79,4 +79,47 @@ class eppDnssecInfoDomainResponse extends eppInfoDomainResponse
         }
         return null;
     }
+
+    /**
+     * Retrieve the keyData elements from the info response
+     *
+    <secDNS:dsData>
+        <secDNS:keyTag>12345</secDNS:keyTag>
+        <secDNS:alg>3</secDNS:alg>
+        <secDNS:digestType>1</secDNS:digestType>
+        <secDNS:digest>49FD46E6C4B45C55D4AC</secDNS:digest>
+    </secDNS:dsData>
+     * @return array|null
+     */
+    public function getSecdns() {
+        // Check if dnssec is enabled on this interface
+        if ($this->findNamespace('secDNS')) {
+            $xpath = $this->xPath();
+            $result = $xpath->query('/epp:epp/epp:response/epp:extension/secDNS:infData/*');
+            $keys = array();
+            if (count($result) > 0) {
+                foreach ($result as $keydata) {
+                    /* @var $keydata \DOMElement */
+                    // Check if the keyTag element is present. If not, use getKeys()
+                    $test = $keydata->getElementsByTagName('keyTag');
+                    $secdns = new eppSecdns();
+                    $secdns->setKeytag($keydata->getElementsByTagName('keyTag')->item(0)->nodeValue);
+                    $secdns->setDigestType($keydata->getElementsByTagName('digestType')->item(0)->nodeValue);
+                    $secdns->setDigest($keydata->getElementsByTagName('digest')->item(0)->nodeValue);
+                    if ($test->length > 0) {
+                        $flags = $keydata->getElementsByTagName('flags')->item(0)->nodeValue;
+                        $algorithm = $keydata->getElementsByTagName('alg')->item(0)->nodeValue;
+                        $pubkey = $keydata->getElementsByTagName('pubKey')->item(0)->nodeValue;
+                        $secdns->setKey($flags, $algorithm, $pubkey);
+                        $keys[] = $secdns;
+                    } else{
+                        $secdns->setAlgorithm($keydata->getElementsByTagName('alg')->item(0)->nodeValue);
+                        $keys[] = $secdns;
+                    }
+                }
+            }
+            return $keys;
+        }
+        return [];
+    }
 }
